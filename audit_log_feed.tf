97444f1258b6ea3775aad4b76ed9e473e1bf147e
John Anderson <john@stacklet.io>
2025-10-02 16:50:53 -0400

    make resources conditional based on including children

--- audit_log_feed.tf
+++ audit_log_feed.tf
@@ -7,59 +7,74 @@ locals {
   audit_filter = "logName:\"cloudaudit.googleapis.com%2Factivity\""
 }
 
-# Workaround for https://github.com/hashicorp/terraform-provider-google/issues/10811
-# Force recreation of sinks when include_children changes
-resource "terraform_data" "include_children_trigger" {
-  input = var.audit_log_include_children
-}
-
 resource "google_pubsub_topic" "audit_feed" {
   count = var.relay_audit_log ? 1 : 0
   name  = "${local.prefix}audit-feed"
 }
 
-resource "google_logging_organization_sink" "organization_audit_feed" {
-  count       = (var.relay_audit_log && var.organization_id != "") ? 1 : 0
+# Workaround for https://github.com/hashicorp/terraform-provider-google/issues/10811
+# Separate resources for include_children true/false to force recreation on change
+
+resource "google_logging_organization_sink" "organization_audit_feed_with_children" {
+  count       = (var.relay_audit_log && var.organization_id != "" && var.audit_log_include_children) ? 1 : 0
   name        = "${local.prefix}audit-feed"
   description = "Organization level audit logs for Stacklet relay"
 
   org_id           = var.organization_id
   filter           = local.audit_filter
-  include_children = var.audit_log_include_children
+  include_children = true
 
   destination = "pubsub.googleapis.com/${google_pubsub_topic.audit_feed[0].id}"
-
-  lifecycle {
-    replace_triggered_by = [terraform_data.include_children_trigger]
-  }
 }
+
+resource "google_logging_organization_sink" "organization_audit_feed_without_children" {
+  count       = (var.relay_audit_log && var.organization_id != "" && !var.audit_log_include_children) ? 1 : 0
+  name        = "${local.prefix}audit-feed"
+  description = "Organization level audit logs for Stacklet relay"
+
+  org_id           = var.organization_id
+  filter           = local.audit_filter
+  include_children = false
+
+  destination = "pubsub.googleapis.com/${google_pubsub_topic.audit_feed[0].id}"
+}
+
 resource "google_pubsub_topic_iam_member" "organization_feed_publisher" {
   count  = (var.relay_audit_log && var.organization_id != "") ? 1 : 0
   topic  = google_pubsub_topic.audit_feed[0].name
   role   = "roles/pubsub.publisher"
-  member = google_logging_organization_sink.organization_audit_feed[0].writer_identity
+  member = var.audit_log_include_children ? google_logging_organization_sink.organization_audit_feed_with_children[0].writer_identity : google_logging_organization_sink.organization_audit_feed_without_children[0].writer_identity
 }
 
-resource "google_logging_folder_sink" "folder_feed" {
-  count       = var.relay_audit_log ? length(var.folder_ids) : 0
+resource "google_logging_folder_sink" "folder_feed_with_children" {
+  count       = (var.relay_audit_log && var.audit_log_include_children) ? length(var.folder_ids) : 0
   name        = "${local.prefix}folder-audit-feed-${var.folder_ids[count.index]}"
   description = "Folder level audit logs for Stacklet relay"
 
   folder           = var.folder_ids[count.index]
   filter           = local.audit_filter
-  include_children = var.audit_log_include_children
+  include_children = true
 
   destination = "pubsub.googleapis.com/${google_pubsub_topic.audit_feed[0].id}"
-
-  lifecycle {
-    replace_triggered_by = [terraform_data.include_children_trigger]
-  }
 }
+
+resource "google_logging_folder_sink" "folder_feed_without_children" {
+  count       = (var.relay_audit_log && !var.audit_log_include_children) ? length(var.folder_ids) : 0
+  name        = "${local.prefix}folder-audit-feed-${var.folder_ids[count.index]}"
+  description = "Folder level audit logs for Stacklet relay"
+
+  folder           = var.folder_ids[count.index]
+  filter           = local.audit_filter
+  include_children = false
+
+  destination = "pubsub.googleapis.com/${google_pubsub_topic.audit_feed[0].id}"
+}
+
 resource "google_pubsub_topic_iam_member" "folder_feed_publisher" {
   count  = var.relay_audit_log ? length(var.folder_ids) : 0
   topic  = google_pubsub_topic.audit_feed[0].name
   role   = "roles/pubsub.publisher"
-  member = google_logging_folder_sink.folder_feed[count.index].writer_identity
+  member = var.audit_log_include_children ? google_logging_folder_sink.folder_feed_with_children[count.index].writer_identity : google_logging_folder_sink.folder_feed_without_children[count.index].writer_identity
 }
 
 resource "google_logging_project_sink" "project_feed" {
